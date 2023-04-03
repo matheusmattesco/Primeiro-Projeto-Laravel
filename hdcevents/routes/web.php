df6e509791262feda4d946928c15818b3d80a7d9
Matheus Matttesco <matheus.mattesco@edu.unifil.br>
2023-04-03 13:57:42 -0300

    Criação

---
+++ hdcevents/routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+
+    $nome = "Matheus";
+    $idade = 29;
+
+    $arr = [1,2,3,4,5]; 
+
+    $nomes = ["matheus","Maria","João","Saulo"];
+
+    return view('welcome', 
+    [
+        'nome' => $nome, 
+        'idade' => $idade, 
+        'profissao' => "Programador",
+        'arr' => $arr,
+        'nomes' => $nomes
+
+    ]);
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/produtos', function () {
+    return view('products');
+});
