a505ac3d5143f618874df6aa32eecefcc24c9e1b
Matheus Matttesco <matheus.mattesco@edu.unifil.br>
2023-04-05 11:19:57 -0300

    Upload de Imagens

--- hdcevents/resources/views/welcome.blade.php
+++ hdcevents/resources/views/welcome.blade.php
@@ -6,27 +6,27 @@
 
 
 <div id="search-container" class="col-md-12">
- <h1>Busque um evento</h1>
+  <h1>Busque um evento</h1>
   <form action="">
-    <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
+      <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
   </form>
 </div>
-<div id="events-container" class="cik-md-12">
-    <h2>Próximos Eventos</h2>
-    <p class="subtitle">Veja os eventos dos próximos dias</p>
-    <div id="cards-container" class="row">
-        @foreach ($events as $event)
-        <div class="card col md-3">
-           <img src="/img/event_placeholder.jpg" alt="{{ $event-> title }}">
-           <div class="card-body">
-             <p class="card-date">10/09/2023</p>
-             <h5 class="card-title">{{ $event-> title}}</h5>
-             <p class="card-participants">X participantes</p>
-             <a href="#" class="btn btn-primary">Saber mais</a>
-            </div> 
-        </div>            
-        @endforeach
-    </div>
+<div id="events-container" class="col-md-12">
+  <h2>Próximos Eventos</h2>
+  <p class="subtitle">Veja os eventos dos próximos dias</p>
+  <div id="cards-container" class="row">
+      @foreach($events as $event)
+      <div class="card col-md-3">
+          <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
+          <div class="card-body">
+              <p class="card-date">10/09/2020</p>
+              <h5 class="card-title">{{ $event->title }}</h5>
+              <p class="card-participants">X Participantes</p>
+              <a href="#" class="btn btn-primary">Saber mais</a>
+          </div>
+      </div>
+      @endforeach
+  </div>
 </div>
 
 <!-- comentario HTML (Aparece no inspecionar) -->
--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -17,9 +17,11 @@ use App\Http\Controllers\EventController;
 
 Route::get('/',[EventController::class, 'index']);
 Route::get('/events/create',[EventController::class, 'create']);
+Route::post('/events',[EventController::class, 'store']);
 
 Route::get('/contact', function () {
     return view('contact');
 });
+
 
 
