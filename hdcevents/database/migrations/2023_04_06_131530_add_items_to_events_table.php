012a97c2f72452193382738c20f4a2ae3dc7089e
Matheus Matttesco <matheus.mattesco@edu.unifil.br>
2023-04-06 10:21:31 -0300

    Funcionando!

--- hdcevents/database/migrations/2023_04_06_131530_add_items_to_events_table.php
+++ hdcevents/database/migrations/2023_04_06_131530_add_items_to_events_table.php
@@ -4,29 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddImageToEventsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::table('events', function (Blueprint $table) {
-            $table->string('image')->nullable();;
+            $table->json('items');
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::table('events', function (Blueprint $table) {
-            $table->dropColumn('image');
+            $table->dropColumn('items');
         });
     }
-}
+};
--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -8,16 +8,17 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-use App\Http\Controllers\EventController; 
+use App\Http\Controllers\EventController;
 
-Route::get('/',[EventController::class, 'index']);
-Route::get('/events/create',[EventController::class, 'create']);
-Route::post('/events',[EventController::class, 'store']);
+Route::get('/', [EventController::class, 'index']);
+Route::get('/events/create', [EventController::class, 'create']);
+Route::get('/events/{id}', [EventController::class, 'show']);
+Route::post('/events', [EventController::class, 'store']);
 
 Route::get('/contact', function () {
     return view('contact');
