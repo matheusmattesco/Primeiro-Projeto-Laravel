118a01ea87e0d7d31215eac16a547790c1d18559
Matheus Matttesco <matheus.mattesco@edu.unifil.br>
2023-04-06 13:26:04 -0300

    Dia06

--- hdcevents/app/Http/Controllers/EventController.php
+++ hdcevents/app/Http/Controllers/EventController.php
@@ -11,9 +11,21 @@ class EventController extends Controller
     
     public function index() {
 
+        $search = request('search');
+
+        if($search) {
+
+            $events = Event::where([
+                ['title','like', '%'.$search.'%']
+            ])->get();
+
+        } else {
+        
         $events = Event::all();
+
+    }
     
-        return view('welcome',['events' => $events]);
+        return view('welcome',['events' => $events, 'search' => $search]);
 
     }
 
@@ -26,9 +38,11 @@ class EventController extends Controller
         $event = new Event;
 
         $event->title = $request->title;
+        $event->date = $request->date;
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
+        $event->items = $request->items;
 
         // Image Upload
         if($request->hasFile('image') && $request->file('image')->isValid()) {
